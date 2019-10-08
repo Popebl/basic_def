b5cf8bef06ac93dbb01778710e0de1144dc772b4
Pope B.Lei <pope.b.lei@gmail.com>
2019-10-08 21:34:24 +0800

    init
    initial

---
+++ c_type_def.h
@@ -0,0 +1,23 @@
+
+
+#if CPU_WORD_LENTH == 64
+typedef unsigned int u_int64
+typedef unsigned short u_int32
+typedef unsigned char u_int8
+typedef int int64
+typedef short int32
+typedef char int8
+#endif
+
+
+#if CPU_WORD_LENTH == 32
+typedef unsigned long u_int64
+typedef unsigned int u_int32
+typedef unsigned short u_int16
+typedef unsigned char u_int8
+typedef long int64
+typedef int int32
+typedef short int16
+typedef char int8
+#endif
+
